bc0811070f7c496ba38e5dee8e43d65520b40962
Alex Saltstein <asaltste@gmail.com>
2021-03-09 12:35:14 -0500

    finished implementation

--- GithubAPI.py
+++ GithubAPI.py
@@ -1,8 +1,32 @@
 import requests
 import json
 
+def repoToString(name, commits):
+    return 'Repo: ' + name + ' Number of commits: ' + str(commits)
+
 def queryGithub(id):
-    print(id)
+    response = requests.get('https://api.github.com/users/'+id+'/repos')
+
+    output = []
+
+    if response.status_code != 200:
+        print('There is no github account with the id: ' + id)
+        return []
+    
+    jsonRes = response.json()
+
+    if len(jsonRes) == 0: 
+        print('No repositories found for the user with id: ' + id)
+        return []
+
+    for repo in jsonRes:
+        name = repo['name']
+        commits = len(requests.get('https://api.github.com/repos/'+id+'/'+name+'/commits').json())
+        temp = repoToString(name, commits)
+        print(temp)
+        output.append(temp)
+
+    return output
 
 if __name__ == '__main__':
     userId = input('Enter Github user ID: ')
--- TestGithubAPI.py
+++ TestGithubAPI.py
@@ -3,11 +3,12 @@ import unittest
 from GithubAPI import queryGithub
 
 class TestGithubAPI(unittest.TestCase):
-    def testGithub(self):
-        self.assertEqual(queryGithub('?'), None)
-    def testGithub2(self):
-        self.assertEqual(queryGithub('asaltste'), None)
-       
+    def testGithubNone(self):
+        self.assertEqual(queryGithub('?'), [])
+    def testGithubNone2(self):
+        self.assertEqual(queryGithub('asaltste'), [])
+    def testGithubNone2(self):
+        self.assertEqual(len(queryGithub('alexsaltstein')), 16)
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
