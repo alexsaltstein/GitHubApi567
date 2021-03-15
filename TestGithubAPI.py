8e46c9247ddeee2134541d4cab88ed03b66ef81e
Alex Saltstein <asaltste@gmail.com>
2021-03-15 14:03:21 -0400

    mock

--- TestGithubAPI.py
+++ TestGithubAPI.py
@@ -1,16 +1,22 @@
 import unittest
+from unittest import mock
 
-from GithubAPI import queryGithub
+import GithubAPI
+
+@mock.patch('GithubAPI.queryGithub')
+def mock_query_github(mock_query_github):
+    mock_query_github.return_value = []
 
 class TestGithubAPI(unittest.TestCase):
     def testGithubNone(self):
-        self.assertEqual(queryGithub('?'), [])
+        
+        self.assertEqual(GithubAPI.queryGithub('?'), [])
     def testGithubNone2(self):
-        self.assertEqual(queryGithub('asaltste'), [])
+        self.assertEqual(GithubAPI.queryGithub('asaltste'), [])
     def testGithubSelf(self):
-        self.assertEqual(len(queryGithub('alexsaltstein')), 16)
+        self.assertEqual(len(GithubAPI.queryGithub('alexsaltstein')), 16)
     def testNoId(self):
-        self.assertEqual(queryGithub(''), [])
+        self.assertEqual(GithubAPI.queryGithub(''), [])
 
 if __name__ == '__main__':
     print('Running unit tests')
